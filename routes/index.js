56a240ddce051d222c668e936d4a0b7703f66a2b
Beth <beth_panx@hotmail.com>
2015-10-04 19:23:20 -0400

    Added 'email us' form section.

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+exports.contact = function(req, res){
+    res.render('contact', { title: 'I dont know if this works - Contact', page: 'contact' })
+};
 module.exports = router;
